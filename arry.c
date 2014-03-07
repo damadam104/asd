3dafeaa115a9fa01ba30dbce5b04f72efc5354f4
D Adam Branson <damadam104@users.noreply.github.com>
2014-03-06 18:50:32 -0600

    Create arry.c

---
+++ arry.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main (void)
+{
+    ///make and name arry
+    int numbers [10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    for (int i = 0; i < 9; i++)
+    {
+    printf ("%i\n", numbers [i]);
+    }
+}    
+//// this here is to show arry of numbers then print the numbers 1 at time
+
